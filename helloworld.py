1f589221802665ef6e899274fbe7d799e4e050fb
Shreya <shreyaleo57@gmail.com>
2023-05-22 23:55:37 +0530

    Merged second.py and helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,7 @@
 name = input('Enter you name ')
 print("Hello " + name + ' !!!')
+number = int(input("Enter a number : "))
+if number%2 == 0:
+    print("The number is even")
+else: 
+    print("The number is odd")
